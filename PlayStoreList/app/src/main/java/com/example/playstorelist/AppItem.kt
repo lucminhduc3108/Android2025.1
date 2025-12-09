bdd53c81e387b5d4868f4250b9aee7c9c0d3ade3
lucminhduc3108 <144041824+lucminhduc3108@users.noreply.github.com>
2025-12-09 13:16:51 +0700

    Delete PlayStoreList directory

--- PlayStoreList/app/src/main/java/com/example/playstorelist/AppItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.playstorelist
-
-data class AppItem(
-    val iconResId: Int,
-    val name: String,
-    val category: String,
-    val rating: Double,
-    val sizeMb: Int
-)
--- PlayStoreList/app/src/main/java/com/example/playstorelist/AppSection.kt
+++
@@ -1,6 +0,0 @@
-package com.example.playstorelist
-
-data class AppSection(
-    val title: String,
-    val apps: List<AppItem>
-)
